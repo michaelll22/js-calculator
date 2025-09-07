0e0758df4b7addd9413d414e93e27c42560dbf71
johnmichael <91875751+michaelll22@users.noreply.github.com>
2025-09-07 11:40:21 +0800

    add buttons and event listener for adding value

--- script.js
+++ script.js
@@ -1,32 +1,87 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
-} 
-    
+  return a * b;
+}
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
 
 function operate(numOne, numTwo, operation) {
-    if (operation === "div") {
-        return divide(numOne, numTwo);
-    }
+  if (numOne == null || numTwo == null || operation == null) {
+    return 0;
+  } else if (operation === "+") {
+    console.log(`${numOne} + ${numTwo} = ${add(numOne, numTwo)}`);
+    return add(numOne, numTwo);
+  } else if (operation === "-") {
+    console.log(`${numOne} - ${numTwo} = ${subtract(numOne, numTwo)}`);
+    return subtract(numOne, numTwo);
+  } else if (operation === "x") {
+    console.log(`${numOne} x ${numTwo} = ${multiply(numOne, numTwo)}`);
+    return multiply(numOne, numTwo);
+  } else if (operation === "/") {
+    console.log(`${numOne} / ${numTwo} = ${divide(numOne, numTwo)}`);
+    return divide(numOne, numTwo);
+  }
 }
 
-let numOne = 5;
-let numTwo = 5;
-let operation = "div";
+const cal = document.querySelector(".cal");
+const operators = document.querySelectorAll(".operation");
 
+function getNum() {
+  let value = 0;
+  cal.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      let currentValue = e.target.textContent;
+      value += currentValue;
+      value = Number(value);
+      console.log(value);
+    }
+  });
+  return value;
+}
 
+function calc(operation) {
+  let numOne = getNum();
+  //   numTwo = getNum();
+}
 
-// enter first num
-// select operation: 
-// enter second num: 
+operators.forEach((operator) => {
+  numTwo = operator.addEventListener("click", () => getNum());
+});
+
+// cal.addEventListener("click", (e) => {
+//         if (e.target.tagName === "BUTTON") {
+//             let currentValue = e.target.textContent;
+//             value += currentValue;
+//             value = Number(value);
+//             console.log(value);
+//     if (e.target.querySelector === ".operation") {
+//       console.log("click");
+//     }
+
+//     // if (!numOne) {
+//     //   numOne = value;
+//     //   console.log("NumOne:", numOne);
+//     // } else if (!operation && ["+", "-", "x", "/"].includes(value)) {
+//     //   operation = value;
+//     //   console.log("Operation:", operation);
+//     // } else if (!numTwo) {
+//     //   numTwo = value;
+//     //   console.log("NumTwo:", numTwo);
+
+//     //   // now both numbers and operation are ready
+//     //   console.log(
+//     //     "Result:",
+//     //     operate(Number(numOne), Number(numTwo), operation)
+//     //   );
+//     // }
+//   }
+// });
